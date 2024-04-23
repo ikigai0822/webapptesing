9cd3fad10d2c023499ae6489715f1b94439b0364
ikigai0822 <pranaymayal17@gmail.com>
2024-04-23 23:46:24 +0530

    selenium testing of the upload form done

--- src/test/java/devops/applied/AppTest.java
+++ src/test/java/devops/applied/AppTest.java
@@ -5,23 +5,40 @@ import org.junit.Test;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement; // Add this line
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     WebDriver driver;
 
     @Test
     public void test() {
-        System.setProperty("webdriver.chrome.driver",
-                "D:\\tools\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\tools\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://127.0.0.1:5500/upload.html");
-        // Assert.assertEquals("BasicKeyboardInterfaceTest", driver.getTitle());
-        // driver.findElement(By.id("textInput")).sendKeys("Pranay");
+
+        // Fill out the form
+        WebElement fileInput = driver.findElement(By.id("torrent-file"));
+        fileInput.sendKeys("C:\\Users\\prana\\Downloads\\chromedriver-win64 (1).zip"); // Replace with the actual path
+                                                                                       // to your file
+
+        WebElement descriptionInput = driver.findElement(By.id("description"));
+        descriptionInput.sendKeys("This is a test description");
+
+        WebElement sizeInput = driver.findElement(By.id("size"));
+        sizeInput.sendKeys("100.00");
+
+        WebElement categoryInput = driver.findElement(By.id("category"));
+        categoryInput.sendKeys("Software");
+
+        WebElement typeInput = driver.findElement(By.id("type"));
+        typeInput.sendKeys("Torrent");
+
+        // Submit the form
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
     }
 
     @After
